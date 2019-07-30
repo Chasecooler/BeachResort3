4a75bf5e539d3c7d5061ae9ba4f32a9971f1a62b
Chasecooler <chasemarq@yahoo.com>
2019-07-30 15:38:07 -0400

    index js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RoomProvider } from "./context";
+
+import { BrowserRouter } from "react-router-dom";
+
+
+
+ReactDOM.render(
+
+  <RoomProvider>
+
+    <BrowserRouter>
+
+      <App />
+
+    </BrowserRouter>
+
+  </RoomProvider>,
+
+  document.getElementById("root")
+
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
